79ae502eb68e7f36d078b608a61ee8c7a53c9a40
Daniel Mather <daniel_mather@trimble.com>
2017-07-12 11:44:40 -0600

    Update version to represent upstream version then package iteration

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake, tools
 class JanssonConan(ConanFile):
     name = "libjansson"
     description = "jansson C JSON parsing library"
-    version = "1.0.0"
+    version = "2.10-1"
     license = "MIT"
     url = "https://github.com/geotracsystems/jansson"
     settings = "os", "compiler", "build_type", "arch"
